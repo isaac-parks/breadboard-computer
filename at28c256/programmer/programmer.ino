04dc88c470f2b3f85fa515fb7027c82aacb74d31
Isaac Parks <72621520+isaac-parks@users.noreply.github.com>
2024-03-12 09:00:44 -0500

    Update programmer.ino

--- at28c256/programmer/programmer.ino
+++ at28c256/programmer/programmer.ino
@@ -1,4 +1,8 @@
 // PROOF OF CONCEPT
+// Using an arduino with at least 12 io lines
+// Address lines will have to manual switches since I don't have a bigger board, which isn't very functional, but could look pretty sick https://www.digikey.com/en/products/detail/cit-relay-and-switch/ANT11SEBQE/12503360
+// Might as well make the data lines manual as well 
+// Programmer for at28c256 eeprom
   int WE = 13;
   int OE = 2;
   int CE = 12;
@@ -12,18 +16,16 @@
   int D7 = 10;
 
   int dl [] = {D0, D1, D2, D3, D4, D5, D6, D7};
-  // bool dl_all_on = true;
-
 void setup() {
   pinMode(WE, OUTPUT);
   pinMode(CE, OUTPUT);
   pinMode(OE, OUTPUT);
-  digitalWrite(WE, HIGH);
+  digitalWrite(WE, HIGH); // Start with the eeprom off (Active Low)
   digitalWrite(CE, HIGH);
   digitalWrite(OE, HIGH);
   Serial.begin(9600);
 
-  delay(5000);
+  delay(5000); // Arduino does some wacky stuff when it starts, setting a delay to let everything settle before writing
   write();
 }
 
@@ -31,14 +33,13 @@ void loop() {
   read();
 }
 
-
 void write() {
   for (int i : dl) {
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
-  }
+  } // Make sure all data lines are off before beginning a write
   delay(10);
-  digitalWrite(D0, HIGH);
+  digitalWrite(D0, HIGH); // This will be replaced by some code that reads the manual data lines switches since the add lines will be manual anyway, for now just hardcoding the write as a pof
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
   digitalWrite(D3, HIGH);
@@ -47,19 +48,17 @@ void write() {
   digitalWrite(D6, LOW);
   digitalWrite(D7, HIGH);
 
-
   digitalWrite(OE, HIGH); // Disable output
   digitalWrite(CE, LOW); // Enable chip
-  delay(1); // Wait tCS - Chip Select Setup Time
+  delay(1); // Chip Enable Setup Time
   digitalWrite(WE, LOW); // Begin write
-  delay(1); // Wait tWP - Write Pulse Width
+  delay(1); // Write Pulse Width
   digitalWrite(WE, HIGH); // End write
-  digitalWrite(CE, HIGH); // Disable chip
   digitalWrite(OE, HIGH); // Disable out
+  digitalWrite(CE, HIGH); // Disable chip
 }
 
-
-void read() {
+void read() { // Could make a switch to toggle read mode... 
   byte data = 0; // Variable to store the read byte
   for (int i : dl) {
     pinMode(i, OUTPUT);
@@ -77,38 +76,13 @@ void read() {
   digitalWrite(OE, LOW); // Enable output
   digitalWrite(CE, LOW); // Enable chip
   delay(1); // Small delay to allow signals to settle and data to become valid
-
   // Read all data lines
   for (int i = 0; i < 8; i++) {
     if (digitalRead(dl[i])) {
       data |= (1 << i);
     }
   }
-
-
   // End read sequence
   digitalWrite(CE, HIGH); // Disable chip
   digitalWrite(OE, HIGH); // Disable output
-
-  // Print the read byte
-
 }
-
-
-
-// void nothing() {
-//     t.setInterval([]() {
-//     toggle_data_lines();
-//   }, 2000);
-// }
-
-// void toggle_data_lines() {
-//     int write_mode = dl_all_on ? HIGH : LOW; 
-//     Serial.println(write_mode);
-//     for (int i : dl) {
-//     pinMode(i, OUTPUT);
-//     digitalWrite(i, write_mode);
-//   }
-
-//   dl_all_on = !dl_all_on;
-// }
